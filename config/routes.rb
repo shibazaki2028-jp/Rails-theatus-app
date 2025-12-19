3cb64959240978e6f0f3d29ef448a123458d63ff
Hiroto Shibazaki <ne241148@senshu-u.jp>
2025-12-19 17:42:45 +0900

    ルーティング設定_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,55 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "top#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :movies, only: [:iudex, :show] do
+    get "search", on: :collection #映画検索
+    #映画から上映スケジュールを探す導線
+    resources :schedules, only: [:index]
+  end
+
+  resources :theaters do
+    get "search", on: :collection #映画館検索
+    #映画館から上映スケジュールを探す導線
+    resources :schedules, only: [:index]
+    resources :screens, only: [:index, :show]
+  end
+
+  #上映スケジュールのidを予約画面へ渡すための導線
+  resources :schedules, only: [] do
+    resources :reservations, only: [:new, :create]
+  end
+
+  resources :reservations
+  resource :account
+  resource :password, only: [:show, :edit, :update]
+  resource :session, only: [:create, :destroy]
+
+  #システム管理者の機能
+  namespace :admin do
+    root "top#index"
+    resources :members do
+      get "search", on: :collection
+      resources :reservations
+    end
+    resources :movies
+    resources :theaters, only: [:index, :show] do
+      resources :screens do
+        resources :seats
+      end
+    end
+    resources :prices #料金マスタ(priceテーブル)
+    resources :schedules #各映画館のスケジュールにアクセスできる
+    resources :reservations, only: [:index, :show, :edit, :update, :destroy]
+  end
+
+  #映画館管理者の機能
+  namespace :theater_admin do
+    root "top#index"
+    resources :screens do
+      resources :seats
+    end
+    #自分の担当している映画館のスケジュール・予約のみアクセス可能
+    resources :schedules 
+    resources :reservations, only: [:index, :show, :edit, :update, :destroy]
+  end
 end
