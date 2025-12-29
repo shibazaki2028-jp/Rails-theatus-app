39552b787ddd675269234c9238ba1e305f53585f
Hiroto Shibazaki <ne241148@senshu-u.jp>
2025-12-29 16:47:28 +0900

    Movieモデルのバリデーション作成

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,16 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :category, presence: true
+  validates :published_on, presence: true
+  validates :ended_on, presence: true
+
+  #上映時間は0より大きい数値でなければいけない
+  validates :screening_time, presence: true,
+  numericality: {
+      only_integer: true,
+      greater_than: 0,
+      allow_blank: true
+  }
+
+  validates :publish, inclusion: { in: [true, false] }
 end
