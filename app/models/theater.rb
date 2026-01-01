b931d014e0308d349b65c11ce37124fee06d5089
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-02 00:59:48 +0900

    Theaterモデルのバリデーション作成

--- app/models/theater.rb
+++ app/models/theater.rb
@@ -1,3 +1,10 @@
 class Theater < ApplicationRecord
-    
+    has_many :screens, dependent: :destroy
+
+    validates :name, presence: true
+    validates :address, presence: true
+    validates :telephone, presence: true,
+        format: { with: /\A\d+\z/, allow_blank: true},
+        length: { minimum: 9, maximum: 13, allow_blank: true },
+        uniqueness: true
 end
