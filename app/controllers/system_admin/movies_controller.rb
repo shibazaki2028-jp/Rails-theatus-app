b2dfbfdf5e012299cf042705a227ba5c099c30a3
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-02 13:36:41 +0900

    システム管理者のMovieコントローラ実装

--- app/controllers/system_admin/movies_controller.rb
+++ app/controllers/system_admin/movies_controller.rb
@@ -1,2 +1,50 @@
 class SystemAdmin::MoviesController < ApplicationController
+
+    #一覧表示
+    def index 
+        @movies = Movie.all
+    end
+
+    #詳細表示
+    def show 
+        @movie = Movie.find(params[:id])
+    end
+
+    #新規登録フォーム
+    def new
+        @movie = Movie.new
+    end
+
+    #新規登録を保存
+    def create
+        @movie = Movie.new(params[:movie])
+        if @movie.save
+            redirect_to :root, notice: "映画の登録が完了しました。"
+        else
+            render "new"
+        end
+    end
+
+    #更新フォーム
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+
+    #内容を保存
+    def update
+        @movie = Movie.find(params[:id])
+        if @movie.save
+            redirect_to :root, notice: "映画の更新が完了しました。"
+        else
+            render "edit"
+        end
+    end
+
+    #映画の削除
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to :root, notice: "映画の削除が完了しました。"
+    end
+
 end
