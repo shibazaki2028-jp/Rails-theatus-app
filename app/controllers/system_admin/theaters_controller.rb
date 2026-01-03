4a6d406501f7d4801a84f6eae7d88324067f83a3
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-04 02:18:48 +0900

    システム管理者のTheaterモデルのアクション実装

--- app/controllers/system_admin/theaters_controller.rb
+++ app/controllers/system_admin/theaters_controller.rb
@@ -1,3 +1,23 @@
 class TheatersController < ApplicationController
-    
+    def index
+        @theaters = Theater.all
+    end
+
+    def show
+        @theater = Theater.find(params[:id])
+    end
+
+    def new
+        @theater = Theater.new
+    end
+
+    def create
+        @theater = Theater.new(params[:theater])
+        if @theater.save
+            redirect_to system_admin_theaters_path, notice: "映画館の登録が完了しました。"
+        else
+            render "new"
+        end
+    end
+
 end
