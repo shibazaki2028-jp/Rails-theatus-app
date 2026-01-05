8606f80d0c8f80dd677e26b33169ec5e5edc3725
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-05 17:54:38 +0900

    上映スケジュールの重複チェック

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -7,6 +7,8 @@ class Schedule < ApplicationRecord
     validates :screened_at, presence: true
     validates :ended_at, presence: true
 
+    validates :schedule_overlap
+
     before_validation :set_ended_at
 
     #上映終了時間は開始時刻から映画の上映時間プラス20分で計算して設定する
@@ -15,4 +17,17 @@ class Schedule < ApplicationRecord
             self.ended_at = screened_at + (movie.screening_time + 20).minutes
         end
     end
+
+    def schedule_overlap
+        overlap = Schedule
+            .where(screen_id: screen_id)
+            .where.not(id: id)
+            .where("screened_at < ? AND ended_at > ? ", ended_at, screened_at)
+            .exists?
+        
+        if overlap
+            errors.add(:screened_at, "が他の上映時間と重複しています")
+        end
+    end
+
 end
