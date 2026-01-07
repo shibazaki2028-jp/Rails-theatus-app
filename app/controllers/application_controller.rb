8b412684a1472060c1c2a4e3e95937d2174f800f
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-08 06:10:56 +0900

    ログイン中アカウントの定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    #before_action :update_expiration_time
+
+    private def current_account
+        Account.find_by(id: cookies.signed[:account_id]) if cookies.signed[:account_id]
+    end
 end
