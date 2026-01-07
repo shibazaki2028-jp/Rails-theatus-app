2558640f514f5fadad4d261e8c1d5ac2d91997d5
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-08 05:46:23 +0900

    roleによる役職の定義・予約との紐付け

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ApplicationRecord
     has_secure_password
 
-    #has_many :reservations
+    has_many :reservations
     #has_many :administrators
 
     validates :user_name, presence: true
@@ -10,4 +10,6 @@ class Account < ApplicationRecord
     attr_accessor :current_password
     validates :password, presence: { if: :current_password }
     validates :role, presence: true
+
+    enum role: { system_admin: 0, theater_admin: 1, general: 2 }
 end
