d35305b26d8a5edce3b40690966102868e1b1358
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-08 06:07:46 +0900

    sessions_controllerの作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+# app/controllers/sessions_controller.rb
+class SessionsController < ApplicationController
+    def new
+      # ログインフォーム表示
+    end
+  
+    def create
+        account = Account.find_by(name: params[:name])
+        if account&.authenticate(params[:password])
+            cookies.signed[:account_id] = {
+                value: account.id,
+                expires: 1.day.from_now
+            }
+        else
+            flash.alert = "名前とパスワードが一致しません"
+        end
+        redirect_to :root
+    end
+
+    def destroy
+        cookies.delete(:account_id)
+        redirect_to :root
+    end
+  end
