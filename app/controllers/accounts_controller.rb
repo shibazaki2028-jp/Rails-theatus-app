7df36a263cfe3051eb8a789b2b57c329457b07c4
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-09 04:06:38 +0900

    システム管理者によるアカウントの新規作成修正

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,12 +4,16 @@ class AccountsController < ApplicationController
   end
 
   def create
-    @account = Account.new(params[:account])
-    @account.role = 2
+    @account = Account.new(account_params)
+    #@account.role = 2
     if @account.save
       redirect_to root_path, notice: "アカウントを登録しました"
     else
       render "new"
     end
+  end
+
+  def account_params
+    params.require(:account).permit(:user_name, :email, :password, :role)
   end
 end
