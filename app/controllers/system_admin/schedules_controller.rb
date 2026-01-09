d554658b44afc1e4a9cbc7e39479623a6536dc97
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-09 09:01:33 +0900

    上映スケジュール一覧・編集ビューファイル実装

--- app/controllers/system_admin/schedules_controller.rb
+++ app/controllers/system_admin/schedules_controller.rb
@@ -5,10 +5,15 @@ class SystemAdmin::SchedulesController < ApplicationController
         @schedule = Schedule.new
     end
 
+    def index
+        @schedules = Schedule.all.includes(:movie, screen: :theater).order(:screened_at)
+        @schedules_by_movie = @schedules.group_by(&:movie)
+    end
+
     def create
         @schedule = Schedule.new(schedule_params)
         if @schedule.save
-            redirect_to system_admin_screens_path, notice: "スケジュールを登録しました"
+            redirect_to system_admin_schedules_path, notice: "スケジュールを登録しました"
         else
             render :new
         end
@@ -22,7 +27,7 @@ class SystemAdmin::SchedulesController < ApplicationController
         @schedule = Schedule.find(params[:id])
         @schedule.assign_attributes(params[:schedule])
         if @schedule.save
-            redirect_to system_admin_screens_path, notice: "スケジュールの更新が完了しました。"
+            redirect_to system_admin_schedules_path, notice: "スケジュールの更新が完了しました。"
         else
             render "edit"
         end
@@ -31,7 +36,7 @@ class SystemAdmin::SchedulesController < ApplicationController
     def destroy
         @schedule = schedule.find(params[:id])
         @schedule.destroy
-        redirect_to system_admin_screens_path, notice: "スケジュールの削除が完了しました。"
+        redirect_to system_admin_schedules_path, notice: "スケジュールの削除が完了しました。"
     end
 
     def schedule_params
