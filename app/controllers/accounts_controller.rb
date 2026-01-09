7b8bdba91caa60d282ec854873c99d2c36417026
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-09 15:58:24 +0900

    マイページ実装

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,23 @@
 class AccountsController < ApplicationController
+  
+  def show
+    @account = current_account
+  end
+
+  def edit
+    @account = current_account
+  end
+
+  def update
+    @account = current_account
+    @account.assign_attributes(params[:account])
+    if @account.save
+      redirect_to :account, notice: "アカウント情報を更新しました。"
+    else
+      render "edit"
+    end
+  end
+
   def new
     @account = Account.new
   end
