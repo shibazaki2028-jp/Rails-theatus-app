d3a660f633b8b3bbdefc470116c1667bd1810ce4
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-16 05:59:50 +0900

    映画館の編集画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
       resources :reservations
     end
     resources :movies
-    resources :theaters, only: [:index, :show, :new, :create] do
+    resources :theaters do#, only: [:index, :show, :new, :create, :edit] do
       resources :screens do
         resources :seats
       end
