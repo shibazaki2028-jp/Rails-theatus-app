d2d2092aad6f5d3eaf3b0ddd64602c0a16658242
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-20 03:21:35 +0900

    ログイン要求時にユーザ名を求めないように変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,15 @@ class SessionsController < ApplicationController
     end
   
     def create
-        account = Account.find_by(user_name: params[:session][:user_name])
-        if account&.authenticate(params[:session][:password])
+        account = Account.find_by(email: params[:email])
+        if account&.authenticate(params[:password])
             cookies.signed[:account_id] = {
                 value: account.id,
                 expires: 1.day.from_now
             }
             redirect_to :root
         else
-            flash.alert = "名前とパスワードが一致しません"
+            flash.alert = "メールアドレスとパスワードが一致しません"
             render :new
         end
     end
