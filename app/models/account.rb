0e2ddfa349d50041bbe47ca2a263fea85e39dadc
Hiroto Shibazaki <ne241148@senshu-u.jp>
2026-01-22 15:44:57 +0900

    バリデーションエラー時の表示修正・メールアドレスの空白を許容しない変更

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,7 +8,7 @@ class Account < ApplicationRecord
     accepts_nested_attributes_for :administrator
 
     validates :user_name, presence: true
-    validates :email, email: { allow_blank: true }
+    validates :email, email: { allow_blank: true }, presence: true
 
     attr_accessor :current_password
     validates :password, presence: { if: :current_password }
